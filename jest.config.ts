b8c04a9e203e42022f5524c27f48dd9cdc234fbb
EmersonLaranja <emersonlaranja@gmail.com>
2024-02-16 08:29:34 -0300

    refact: exclude main from jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "jest";
 
 const config: Config = {
   collectCoverage: false,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   preset: "@shelf/jest-mongodb",
